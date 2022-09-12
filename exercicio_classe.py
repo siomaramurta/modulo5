e01e98c5419025fdb71539d5023f069df28e28e7
siomaramurta <siomara.murta@gmail.com>
2022-09-11 21:25:38 -0300

    alteração do atributo 'classes' para 'tarifas'

--- exercicio_classe.py
+++ exercicio_classe.py
@@ -1,6 +1,6 @@
 class PassagemAerea:
-    def __init__(self, classes, vantagens):
-        self.classes = classes
+    def __init__(self, tarifas, vantagens):
+        self.tarifas = tarifas
         self.vantagens = vantagens
 
     def tipo_de_assento(self, local_assento):
@@ -10,37 +10,37 @@ class PassagemAerea:
         return "o valor da tarifa é muito alto"
 
 class ClasseEconomy(PassagemAerea):
-    def __init__(self, classes, vantagens, tipo_de_trecho):
+    def __init__(self, tarifas, vantagens, tipo_de_trecho):
         self.tipo_de_trecho = tipo_de_trecho
-        super().__init__(classes, vantagens)
+        super().__init__(tarifas, vantagens)
 
     def preco(self):
         return format("é a classe que tem as tarifas mais baratas.")
 
 class PremiumEconomy(PassagemAerea):
-    def __init__(self, classes, vantagens):
-        super().__init__(classes, vantagens)
+    def __init__(self, tarifas, vantagens):
+        super().__init__(tarifas, vantagens)
 
     def preco(self):
         return format("é a classe com tarifa de valor intermediário.")
 
 class ClasseExecutiva(PassagemAerea):
-    def __init__(self, classes, vantagens):
-        super().__init__(classes, vantagens)
+    def __init__(self, tarifas, vantagens):
+        super().__init__(tarifas, vantagens)
     
     def preco(self):
         return format("é uma das tarifas mais caras que existe.")
 
 class PrimeiraClasse(PassagemAerea):
-    def __init__(self, classes, vantagens):
-        super().__init__(classes, vantagens)
+    def __init__(self, tarifas, vantagens):
+        super().__init__(tarifas, vantagens)
 
 classe1 = ClasseEconomy('Classe Econômica', 'preço e simplicidade', 'nacionais e internacionais')
-print('A classe mais básica é a', classe1.classes, 'suas vantagens são', classe1.vantagens,'. É possível voar nessa classe em trechos ', classe1.tipo_de_trecho)
+print('A classe mais básica é a', classe1.tarifas, 'suas vantagens são', classe1.vantagens,'. É possível voar nessa classe em trechos ', classe1.tipo_de_trecho)
 print('Essa classe', classe1.preco())
 
 classe2 = PremiumEconomy('Premium Economy', 'preço e confortos extras')
-print('A segunda classe mais básica é a', classe2.classes, 'suas vantagens são', classe2.vantagens,'.')
+print('A segunda classe mais básica é a', classe2.tarifas, 'suas vantagens são', classe2.vantagens,'.')
 print('Essa classe', classe2.preco())
 
 classe3 = ClasseExecutiva('Classe Executiva', 'a viagem é ainda mais confortável e personalizada, com espaços privativos e muito mais opções de relaxamento, gastronomia e entretenimento durante o voo')
